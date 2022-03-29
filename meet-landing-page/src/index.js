1b9842f78c59f7ec04ecda844e5328d4fac01468
Radu Dan Chiorean <chioreanradudan@gmail.com>
2022-03-29 20:54:36 +0300

    Remove routing in meet-landing-page

--- meet-landing-page/src/index.js
+++ meet-landing-page/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Normalize } from 'styled-normalize'
 import { theme, GlobalStyle } from './common/styles'
-import DesignSystemPage from './design-sytem/design-system-page'
 import LandingPage from './landing-page/landing-page'
 
 ReactDOM.render(
@@ -12,12 +10,7 @@ ReactDOM.render(
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route path="/design-system" element={<DesignSystemPage />} />
-        </Routes>
-      </Router>
+      <LandingPage />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
